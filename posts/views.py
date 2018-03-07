be66cb75a399bccbe4fdfc987d063fe798c9482d
Born After TV <bornaftertv@gmail.com>
2018-03-07 15:33:20 +1300

    s

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 def index(request):
-    #return HttpResponse('Hello From Posts')
-    return render(request, 'posts/index.html', {
-        'title': 'My DEV Posts'
-    })
+    
+    posts = Posts.objects.all()[:10]
+
+    data = {
+        'title': 'My DEV Posts',
+        'posts': posts
+    }
+
+    return render(request, 'posts/index.html', data)
