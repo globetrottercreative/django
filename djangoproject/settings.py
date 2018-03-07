a3e34fe20e930f9753f4fbb18ec4b858a499a983
Born After TV <bornaftertv@gmail.com>
2018-03-08 11:28:15 +1300

    s

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -108,10 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    ('assets', os.path.join(BASE_DIR, 'static')),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
