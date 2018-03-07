1c881fa99d82a6eec51babe9717f51674148b87c
Born After TV <bornaftertv@gmail.com>
2018-03-07 13:21:57 +1300

    c

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p5r76hi$5cx+c+5&9%2w0j%2w%64k=_^k!am%2ipo$8#6i-yv='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
