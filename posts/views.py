0ccfc170e5f50fff05387a388c53c978310c0156
Born After TV <bornaftertv@gmail.com>
2018-03-07 17:10:17 +1300

    s

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello From Posts')
-    return render(request, 'posts/index.html', {
+    posts = Posts.objects.all()[:10]
+    context = {
         'title': 'Latests Posts'
-    })
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
 
 def details(request, id):
     post = Posts.objects.get(id=id)
