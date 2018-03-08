bc254e82b8f7eb315d3405c5e9593fbf06455167
Born After TV <bornaftertv@gmail.com>
2018-03-08 19:32:02 +1300

    s

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .forms import SearchForm
+from dashboard.models import UserProfile
 # Create your views here.
 def catcher(request):
-
     form = SearchForm()
-
+    user = UserProfile.objects.all()
     data = {
         'name': 'I\'m The Name',
-        'form': form
+        'form': form,
+        'id_name': user.username
     }
     return render(request, 'dashboard/search.html', data)
 
