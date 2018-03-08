aef75a59d831861b243a4b099ea93a16732796f3
Born After TV <bornaftertv@gmail.com>
2018-03-09 07:24:25 +1300

    d

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^$', login, {'template_name': 'dashboard/login.html'}),
-    url(r'^home/$', views.catcher),
+    url(r'^home/$', views.catcher, name='search_crypto'),
+    url(r'^edit/$', views.edit, name='edit_profile'),
     url(r'^register/$', views.registration, name='registration'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import SearchForm, RegistrationForm
 from dashboard.models import UserProfile
+from django.contrib.auth.forms import UserChangeForm
 # Create your views here.
 def catcher(request):
     form = SearchForm()
-    user = UserProfile.objects.all()
+    user = request.user
     print(user)
     data = {
         'name': 'I\'m The Name',
@@ -13,6 +14,18 @@ def catcher(request):
     }
     return render(request, 'dashboard/search.html', data)
 
+def edit(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('search/')
+    else:
+        form = UserChangeForm(instance=request.user)
+        args = {
+            'form': form,
+        }
+        return render(request, 'dashboard/edit_profile.html', args)
 
 def registration(request):
     if request.method == 'POST':
