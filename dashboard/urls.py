f13b2e5e51af5cf23520b935678d0bf5016e88e6
Born After TV <bornaftertv@gmail.com>
2018-03-08 20:58:53 +1300

    r

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth.views import login
 urlpatterns = [
     url(r'^$', login, {'template_name': 'dashboard/login.html'}),
     url(r'^home/$', views.catcher),
+    url(r'^register/$', views.registration, name='registration'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
-from .forms import SearchForm
+from django.shortcuts import render, redirect
+from .forms import SearchForm, RegistrationForm
 from dashboard.models import UserProfile
 # Create your views here.
 def catcher(request):
     form = SearchForm()
     user = UserProfile.objects.all()
+    print(user)
     data = {
         'name': 'I\'m The Name',
         'form': form,
-        'id_name': user.username
+        'id_name': user
     }
     return render(request, 'dashboard/search.html', data)
 
 
+def registration(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home/')
+    else:
+        form = RegistrationForm()
+        args = {
+            'form': form,
+        }
+        return render(request, 'dashboard/register.html', args)
